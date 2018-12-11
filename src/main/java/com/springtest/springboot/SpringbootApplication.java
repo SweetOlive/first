35c113b96d54dfeaf935636f3d907ff21a1465fc
Lings <603656949@qq.com>
2018-12-11 13:12:51 +0800

    配置生成war包

--- src/main/java/com/springtest/springboot/SpringbootApplication.java
+++ src/main/java/com/springtest/springboot/SpringbootApplication.java
@@ -3,10 +3,17 @@ package com.springtest.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.springtest.springboot.dao")
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
